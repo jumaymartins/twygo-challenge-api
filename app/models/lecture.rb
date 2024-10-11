5ba1e776e0feded0a9ba078b34d73215e377a6c2
jumaymartins <julia.mayumi.martins@hotmail.com>
2024-10-11 11:14:22 -0300

    fix: fix rubocop alerts

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,16 +1,16 @@
 class Lecture < ApplicationRecord
   attr_accessor :title, :references
-  
+
   has_one_attached :video, dependent: :destroy
-  
+
   validates :title, :video, presence: true
   validate :validate_video
-  
+
   private
-  
+
   def validate_video
-    if video.attached? && !video.content_type.in?(%w(video/mp4))
-      errors.add(:video, 'Must be a video MP4 format')
+    if video.attached? && !video.content_type.in?(%w[video/mp4])
+      errors.add(:video, "Must be a video MP4 format")
     end
   end
 end
